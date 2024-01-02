8072d355299daa61068ea9a3ef88e492d00470c9
ayalaBiton <133572257+ayalaBiton@users.noreply.github.com>
2024-01-03 00:47:41 +0200

    Add files via upload

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { addPost } from '../API';
+
+
+export const NewPost = ({userId, updatePosts, setShowNewPost, id}) =>
+{
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [validation, setValidation] = useState("");
+
+    //יצירת הודעה חדשה
+    const createPost = async (e) => {
+        e.preventDefault();
+        //:אימות הזנת נתוני המשתמש
+        //השדות הם שדות חובה 
+        if (!title || !body) {
+            setValidation('All fields must be filled');
+        }
+        //השדות חיבות להכיל אותיות באנגלית או העברית
+        else if (!/^[a-zA-Zא-ת\s]+$/.test(title) || !/^[a-zA-Zא-ת\s]+$/.test(body)) {
+            setValidation('Only Hebrew and English letters must be entered');
+        }
+        else
+        {
+            const postJson = {
+                "userId": userId,
+                "id": id,
+                "title": title,
+                "body": body
+            };
+            try {
+                //יצירת הודעה חדשה post קריאה לפונקצית
+                const newPost = await addPost(postJson);
+                updatePosts(newPost);
+                setShowNewPost(false)
+            } 
+            catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
+    return (
+        <>
+            <div id='divNewPost'>
+                {/* טופס יצירצת הודעה חדשה */}
+                <form id='formNewPost' onSubmit={createPost}>
+                    <span onClick={() => setShowNewPost(false)} className='close'>&times;</span>
+                    <p>Create new post</p><br/>
+                    <label><b>Title:</b></label>
+                    <input id='input' type='text' onChange={(e) => setTitle(e.target.value)}></input>
+                    <label><b>Body:</b></label>
+                    <input id='input' type='text' onChange={(e) => setBody(e.target.value)}></input>
+                    {validation && <p>{validation}</p>}
+                    <Button id='btnCreate' variant="contained" type='submit'>Create</Button>
+                </form>
+            </div>
+        </>
+    );
+}
